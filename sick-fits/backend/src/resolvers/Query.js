f55d527a4eda91819439addd43f26d8b2f1e9d17
Alex Ung <alexung.ucsb@gmail.com>
2019-05-04 12:37:47 -0700

    good

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,10 @@
-const Query = {};
+const Query = {
+  // shorthand for function dogs().
+  // ctx gives us access to headers
+  dogs(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    return global.dogs;
+  }
+};
 
 module.exports = Query;
