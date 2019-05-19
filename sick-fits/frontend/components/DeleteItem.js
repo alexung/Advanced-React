07933afe0d28e85a8dcb056b0c1321a59a7b58a9
Alex Ung <alexung.ucsb@gmail.com>
2019-05-19 16:49:47 -0700

    change the schema to add a deleteItem mutation, add a resolver for that mutation which handles deleting the item from the db, and then within DeleteItem.js actually create a mutation component with the button nested inside with an onClick handler to delete the item.  Must also delete from gqls cache on local machine, which happensin the update function also on DeleteItem.js

---
+++ sick-fits/frontend/components/DeleteItem.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+
+import { ALL_ITEMS_QUERY } from "./Items";
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`;
+
+class DeleteItem extends Component {
+  update = (cache, payload) => {
+    console.log({ cache, payload });
+    // manually update the cache on the client, so it matches the server
+    // 1. Read the cache for the items we want
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    console.log({ data, payload });
+    // 2. Filter the deleted item out of the page
+    data.items = data.items.filter(
+      item => item.id !== payload.data.deleteItem.id
+    );
+    // 3. Put the items back!
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
+
+  render() {
+    return (
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => (
+          <button
+            onClick={() => {
+              if (confirm("You sure you'd like to delete this?")) {
+                // this deleteItem is passed in via mutation on line 20
+                deleteItem();
+              }
+            }}
+          >
+            {this.props.children}
+          </button>
+        )}
+      </Mutation>
+    );
+  }
+}
+
+export default DeleteItem;
