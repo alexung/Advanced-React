36772dec23448950fa5a5dac4ac90f941887b741
Alex Ung <alexung.ucsb@gmail.com>
2019-05-30 21:01:13 -0700

    In Items.js, create a way to actually have the correct items show on each page.  Also had :::;;;;;"

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,61 +1,65 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
 
-import Item from './Item';
-import Pagination from './Pagination';
+import Item from "./Item";
+import Pagination from "./Pagination";
+import { perPage } from "../config";
 
 const ALL_ITEMS_QUERY = gql`
-	query ALL_ITEMS_QUERY {
-		items {
-			id
-			title
-			price
-			description
-			image
-			largeImage
-		}
-	}
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
 `;
 
 const Center = styled.div`
-	text-align: center;
+  text-align: center;
 `;
 
 const ItemsList = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 1fr;
-	grid-gap: 60px;
-	max-width: ${props => props.theme.maxWidth};
-	margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
 `;
 
 export default class Items extends Component {
-	render() {
-		return (
-			<div>
-				<Pagination page={this.props.page} />
-				<Query query={ALL_ITEMS_QUERY}>
-					{/** Lots more in the payload vv */}
-					{({ data, error, loading }) => {
-						console.log({ data, error, loading });
-						// should prob check before returning, for loading/error
-						if (loading) return <p>Loading...</p>;
-						if (error) return <p>Error: {error.message}</p>;
-						return (
-							<ItemsList>
-								{data.items.map(item => (
-									<Item item={item} key={item.id} />
-								))}
-							</ItemsList>
-						);
-					}}
-				</Query>
-				<Pagination page={this.props.page} />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Pagination page={this.props.page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{ skip: this.props.page * perPage - perPage }}
+        >
+          {/** Lots more in the payload vv */}
+          {({ data, error, loading }) => {
+            console.log({ data, error, loading });
+            // should prob check before returning, for loading/error
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.message}</p>;
+            return (
+              <ItemsList>
+                {data.items.map(item => (
+                  <Item item={item} key={item.id} />
+                ))}
+              </ItemsList>
+            );
+          }}
+        </Query>
+        <Pagination page={this.props.page} />
+      </div>
+    );
+  }
 }
 
 export { ALL_ITEMS_QUERY };
