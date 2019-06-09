3d8d50f38b74a23e291e1bc589f3f1c841030186
Alex Ung <alexung.ucsb@gmail.com>
2019-06-08 19:05:26 -0700

    add signup to schema.graphql, add signup mutation that actually returns back the signed up user, hash the password properly so its hidden, create a cookie on the response so that everytime user clicks new page the token comes for a ride and page knows the user has logged in, whats in their cart, etc

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,3 +1,4 @@
+const cookieParser = require("cookie-parser");
 // this is the entrypoint to our application, so req .env to get all var's you need
 require("dotenv").config({ path: ".env" });
 const createServer = require("./createServer");
@@ -5,7 +6,21 @@ const db = require("./db");
 
 const server = createServer();
 
-// TODO use express middleware to handle cookies
+// TODO use express middleware to handle cookies.
+// NOTE: middleware is a function that happens between request and response
+/* 
+
+REQ /dogs.html
+
+in the middle, you might want to...
+1. verify auth
+2. transform the dogs
+3. etc
+
+RES /[dog1, dog2]
+
+*/
+server.express.use(cookieParser()); // makes cookies a parsable obj, as opposed to a long string
 // TODO use express middleware to populate current user
 
 server.start(
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,46 +1,80 @@
-// this is for Yoga GraphQL
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+// this is for Yoga GraphQL
+// mutation functions below MUST match up with Mutations in schema.graphql
 const Mutations = {
-	async createItem(parent, args, ctx, info) {
-		// TODO: check if they are logged in
-		// ctx.db accesses db
-		// createItem is a promise
-		const item = await ctx.db.mutation.createItem({
-			data: {
-				...args
-				// we can spread like above, or manually do it below.
-				// title: args.title,
-				// description: args.desc
-			},
-			info
-		}); // pass in info here so that actual item is returned to us from db after we've created it
-		return item;
-	},
-	updateItem(parent, args, ctx, info) {
-		// first take a copy of the updates
-		const updates = { ...args };
-		// remove the ID from the updates
-		delete updates.id;
-		// run the update method
-		return ctx.db.mutation.updateItem(
-			{
-				data: updates,
-				where: {
-					id: args.id
-				}
-			},
-			info
-		);
-	},
-	async deleteItem(parent, args, ctx, info) {
-		const where = { id: args.id };
-		// 1. find the item
-		const item = await ctx.db.query.item({ where }, `{ id title}`);
-		// 2. Check if they own that item, or have the permissions
-		// TODO
-		// 3. Delete it!
-		return ctx.db.mutation.deleteItem({ where }, info);
-	}
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+    // ctx.db accesses db
+    // createItem is a promise
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+        // we can spread like above, or manually do it below.
+        // title: args.title,
+        // description: args.desc
+      },
+      info
+    }); // pass in info here so that actual item is returned to us from db after we've created it
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title}`);
+    // 2. Check if they own that item, or have the permissions
+    // TODO
+    // 3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase all emails for consistency
+    args.email = args.email.toLowerCase();
+    // hash their password.  The integer as second arg to bcrypt.hash() is the salt length...
+    // ... allows for uniqueness! 10 hash is different from 15 hash.
+    const password = await bcrypt.hash(args.password, 10);
+    // second arg to createUser (or any mutation) is what we want the server to return to us
+    args.user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          // everyone that signs up will be a USER
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    );
+    // create the JWT token for them
+    // we need to pass our .env's APP_SECRET here so it can sign it with something specific to our app
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // We set the jwt as a cookie on the response, so everytime signed in user clicks another page, that token comes along for the ride
+    // setting a 'token' cookie
+    ctx.response.cookie("token", token, {
+      // to make sure that only http can access it, so malicious js cannot access this cookie
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+    // Return the user to the browser
+    return user;
+  }
 };
 
 module.exports = Mutations;
